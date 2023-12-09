911c5840e5e1e274300196be03b60839297488b5
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-09 21:20:12 +0530

    changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/tech.jpeg";
+import image from "../images/tech.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
