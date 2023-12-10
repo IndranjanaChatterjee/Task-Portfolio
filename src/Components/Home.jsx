62b82289a8ac3a23a061ea1220b20ac3548b4974
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-10 15:12:42 +0530

    add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -34,7 +34,7 @@ const Home = ({ name, title }) => {
     <section id="home" className="min-height">
       <img src={image} className="background" alt="" />
       <div
-        style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem",fontSize:"5rem" color: "white" }}
+        style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem",fontSize:"5rem" ,color: "white" }}
       >
         <h1>Hii I,m Indranjana Chatterjee {}
           <span style={{fontSize:"7rem"}}>
