4e22637741f5035e3a21ab2e3cfe402e06a26735
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-12 20:14:46 +0530

    responsive

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -60,11 +60,11 @@ const Portfolio = () => {
   return (
     <section className="padding" id="portfolio">
       <h2 style={{ textAlign: "center" }}>Portfolio</h2>
-      <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
+      <div style={{ display: "flex", flexDirection: "row",flexWrap:"wrap", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
             src={image}
-            style={{ height: "90%", width: "100%", objectFit: "cover" }}
+            style={{ height: "40rem", width: "40rem", objectFit: "cover" }}
             alt={imageAltText}
           />
         </div>
