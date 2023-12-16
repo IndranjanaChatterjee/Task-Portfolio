c9dbefebdc4e66e79bc40b207d676f57db8b46a5
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-16 22:08:53 +0530

    check

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -52,7 +52,7 @@ const Home = ({ name, title }) => {
           
         </h1>
         
-        <img src={face} alt="face" />
+        
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
         <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
