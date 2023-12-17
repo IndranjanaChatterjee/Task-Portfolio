9661ad5a2283cb766e354dfbbfaed96e72421fe2
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-17 10:35:13 +0530

    education

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function Education() {
   return (
-    <div id='education'>
-      Education
-    </div>
-  )
+    <section id="education">
+      <h1>Education</h1>
+      <div className="parts">
+        <div className="btech">
+          <h2 className="type">Btech in Computer Science and Technology (2022-2026)</h2>
+          <p>
+            Studing in University of Engineering and Management ,Kolkata with 10.0 CGPA in the
+            second year
+          </p>
+        </div>
+        <div className="isc">
+          <h2 class="type">ISC (Higher Secondary)</h2>
+          <p>Successfully passed ISC with 94.75% with Science</p>
+        </div>
+        <div className="icse">
+          <h2 class="type">ICSE (Secondary)</h2>
+          <p>Successfully passed ISCE with 92% with Science</p>
+        </div>
+      </div>
+    </section>
+  );
 }
