7f57d2d9aed9e1e424edfed9cce32b5dcd1ad55b
Indranjana Chatterjee <indranjanachatterjee@gmail.com>
2023-12-17 11:23:46 +0530

    back

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import image from "../images/eduback.jpg";
 
 export default function Education() {
   return (
     <section id="education">
+      <img className="background" src={image} alt="edu" />
       <h2 className="edu">Education</h2>
       <div className="parts">
         <div className="btech b">
         <FontAwesomeIcon icon="fa-solid fa-graduation-cap" />
-          <h2 className="type">Btech in Computer Science and Technology (2022-2026)</h2>
-          <p>
+          <h3 className="type">Btech in Computer Science and Technology (2022-2026)</h3>
+          <p className="small">
             Studing in University of Engineering and Management ,Kolkata with 10.0 CGPA in the
             second year
           </p>
         </div>
         <div className="isc b">
         <FontAwesomeIcon icon="fa-solid fa-graduation-cap" />
-          <h2 class="type">ISC (Higher Secondary)</h2>
-          <p>Successfully passed ISC with 94.75% with Science</p>
+          <h3 class="type">ISC (Higher Secondary)</h3>
+          <p className="small">Successfully passed ISC with 94.75% with Science</p>
         </div>
         <div className="icse b">
         <FontAwesomeIcon icon="fa-solid fa-graduation-cap" />
-          <h2 class="type">ICSE (Secondary)</h2>
-          <p>Successfully passed ISCE with 92% with Science</p>
+          <h3 class="type">ICSE (Secondary)</h3>
+          <p className="small">Successfully passed ISCE with 92% with Science</p>
         </div>
       </div>
     </section>
